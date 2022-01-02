7dc41e0d2f94e6a45fcb30e503961d91ce722d51
Caden Haustein <code@brightlysalty.33mail.com>
2022-01-02 13:12:06 -0600

    Publish.

--- site.hs
+++ site.hs
@@ -6,7 +6,7 @@ import           Hakyll
 
 --------------------------------------------------------------------------------
 main :: IO ()
-main = hakyll $ do
+main = hakyllWith config $ do
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
@@ -58,6 +58,8 @@ main = hakyll $ do
 
     match "templates/*" $ compile templateBodyCompiler
 
+config :: Configuration
+config = defaultConfiguration { destinationDirectory = "docs" }
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
